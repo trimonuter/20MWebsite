cd827662a2d75d54268b20a935f6e77b9b744b67
trimonuter <mrqaist@gmail.com>
2023-07-22 22:48:32 +0700

    Rebased

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ const main = document.getElementById("main")
 
 /**
  * @typedef {{
- *  title: string,
  *  posterURL: string,
- *  rating: number
+ *  title: string,
+ *  season: string,
+ *  rating: number,
+ *  members: string
  * }} Data
  */
 
@@ -16,14 +18,18 @@ const main = document.getElementById("main")
  * @param {number} count 
  * @param {Data} data 
  */
-function createCard(count, data){
+function createCard(rank, data){
     /** @type {HTMLElement} */
     const clone = cardTemplate.content.cloneNode(true)
 
-    clone.querySelector("[data-count]").innerHTML = count
-    clone.querySelector("[data-title]").innerHTML = data.title
+    clone.querySelector("[data-rank]").innerHTML = rank
     clone.querySelector("[data-poster]").src = data.posterURL
+
+    clone.querySelector("[data-title]").innerHTML = data.title
+    clone.querySelector("[data-season]").innerHTML = data.season
+
     clone.querySelector("[data-rating]").innerHTML = data.rating
+    clone.querySelector("[data-members]").innerHTML = data.members
     return clone
 }
 
@@ -32,13 +38,16 @@ const FILM_LIST = [
     {
         title: "Oppenheimer",
         posterURL: "https://cinemags.org/wp-content/uploads/2023/05/Oppenheimer-poster.jpg",
-        rating: "9.09"
+        rating: "9.09",
+        season: "Fall 2016",
+        members: "1.5M"
     },
     {
         title: "Barbie",
         posterURL: "https://deadline.com/wp-content/uploads/2023/04/barbie-BARBIE_VERT_TSR_W_TALENT_2764x4096_DOM_rgb.jpg?w=800",
-        rating: "9.05"
-
+        rating: "9.05",
+        season: "Summer 2023",
+        members: "1.0M"
     }
 ]
 
