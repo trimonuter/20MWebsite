2e5e87bfb881932cef9a8fe580766eb86fe810a5
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-07-25 23:21:43 +0700

    Created backend, and served static files

--- server.js
+++ server.js
@@ -1,44 +1,50 @@
-const express = require('express');
-const cors = require('cors');
-const puppeteer = require('puppeteer');
-const app = express();
-const port = 3000; // You can use any available port number
+import express from "express"
+import { parse } from "node-html-parser";
+import fetch from "node-fetch";
+import livereload from "livereload"
+import connectLivereload from "connect-livereload"
 
-// Enable CORS to allow requests from the frontend (replace the origin URL with your frontend URL)
-app.use(cors({ origin: 'http://127.0.0.1:5500' }));
+const app = express();
+const port = 3000;
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch('./public');
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+app.use(connectLivereload());
+app.use(express.static("./public"))
 
 // Asynchronous function to scrape anime data
 async function scrapeData() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://myanimelist.net/topanime.php');
+  const res = await fetch("https://myanimelist.net/topanime.php")
+  const text = await res.text()
+  const document = parse(text)
 
-  const getAllAnime = await page.evaluate(() => {
-    const ANIME_LIST = [];
-    const allAnime = document.querySelectorAll(".di-ib.clearfix h3 a");
-    const allPosters = document.querySelectorAll(".hoverinfo_trigger.fl-l.ml12.mr8 img");
-    const allScores = document.querySelectorAll(".score.ac.fs14 span");
-    const allURL = document.querySelectorAll(".hoverinfo_trigger.fl-l.ml12.mr8");
+  const ANIME_LIST = [];
+  const allAnime = document.querySelectorAll(".di-ib.clearfix h3 a");
+  const allPosters = document.querySelectorAll(".hoverinfo_trigger.fl-l.ml12.mr8 img");
+  const allScores = document.querySelectorAll(".score.ac.fs14 span");
+  const allURL = document.querySelectorAll(".hoverinfo_trigger.fl-l.ml12.mr8");
 
-    for (let i = 0; i < allAnime.length; i++) {
-      const data = {
-        title: allAnime[i].innerHTML,
-        posterURL: allPosters[i].getAttribute("src"),
-        rating: parseFloat(allScores[i].innerHTML),
-        season: "Fall 2016",
-        members: "1.5M",
-        URL: allURL[i].getAttribute("href")
-      };
+  for (let i = 0; i < allAnime.length; i++) {
+    const data = {
+      title: allAnime[i].innerHTML,
+      posterURL: allPosters[i].getAttribute("data-src"),
+      rating: parseFloat(allScores[i].innerHTML),
+      season: "Fall 2016",
+      members: "1.5M",
+      URL: allURL[i].getAttribute("href")
+    };
 
-      ANIME_LIST.push(data);
-    }
+    ANIME_LIST.push(data);
+  }
 
-    return ANIME_LIST;
-  });
-
-  await browser.close();
-
-  return getAllAnime;
+  return ANIME_LIST;
 }
 
 // Endpoint to serve the scraped anime data
